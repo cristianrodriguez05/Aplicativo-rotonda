71dc84c2dfebb5d924f2972c80d6c23009166dbb
cristianrodriguez05 <30886461+cristianrodriguez05@users.noreply.github.com>
2022-12-17 17:52:22 -0500

    FINALIZADO PEDIDO

--- principal.py
+++ principal.py
@@ -61,11 +61,11 @@ def home():
             
             #aqui se debe hacer la consulta para saber si si es el usuario o no
             #y aqui añadir una variable para asi saber si se es un admin, taquilla, gerente o cliente
-            if '@adm.com' in resultados[3]:
+            if '@adm.com' in resultados[2]:
                 typeU='Admin'
                 #nombre = consulta para el nombre del admin
                 #cantidad = consulta opara la cantidad de salas
-                nombre=resultados[1]
+                nombre=resultados[0]
                 exist='si'
                 resp = make_response(render_template('homerestaurante.html',typeU=typeU,nombre=nombre))
                 resp.set_cookie('name_user',nombre)
@@ -90,7 +90,46 @@ def homerestaurante():
 
 @app.route('/restaurantepizza', methods = ['POST','GET'])
 def restaurantepizza():
-    return render_template('restaurantepizza.html')
+    cur_01=conexion.cursor()
+    select_menu= "SELECT * FROM menu"
+    cur_01.execute(select_menu)
+    resultados=cur_01.fetchall()
+
+    resultadosjson=[]
+    for r in resultados: 
+        resultadosjson.append({
+                "nombre":r[1],
+            })
+    print(resultadosjson)
+
+    cur_02=conexion.cursor()
+    select_prod= "SELECT * FROM producto"
+    cur_02.execute(select_prod)
+    resultadosprod=cur_02.fetchall()
+
+    resultadosprodjson=[]
+    for r in resultadosprod: 
+        resultadosprodjson.append({
+                "nombre":r[0],
+            })
+    print(resultadosprodjson)
+    return render_template('restaurantepizza.html',resultados=resultadosjson, resultadosprod=resultadosprodjson)
+def pasaramenu():
+    cur_01=conexion.cursor()
+    select_menu= "SELECT * FROM menu"
+    cur_01.execute(select_menu)
+    resultadosmenu=cur_01.fetchall()
+    resultadosmenujson=[]
+    for r in resultadosmenu: 
+            resultadosmenujson.append({
+                "id":r[0],
+                "nombre":r[1],
+                "precio":r[2],
+                "restaurante":r[3],
+            })
+    print(resultadosmenujson)
+    return render_template('pizzamenu.html',resultadosmenu=resultadosmenujson)
+
 
 @app.route('/restaurantehambur', methods = ['POST','GET'])
 def restaurantehambur():
@@ -106,19 +145,165 @@ def restaurantebebida():
 
 @app.route('/agremenu', methods = ['POST','GET'])
 def agremenu():
-    return render_template('agremenu.html')
+    if request.method == 'POST':
+        id_menu = request.form['id_menu']
+        nombre_menu = request.form['nombre_menu']
+        precio_menu = request.form['precio_menu']
+        restaurante_menu = request.form['restaurante_menu']
+        print(id_menu, nombre_menu, precio_menu, restaurante_menu)
+        cur_01=conexion.cursor()
+        insert_datos= "insert into menu (id_menu, nombre_menu, precio_menu, restaurante_menu) VALUES (:1, :2, :3, :4)"
+        print(insert_datos)
+        cur_01.execute(insert_datos,[id_menu, nombre_menu, precio_menu, restaurante_menu])
+        conexion.commit()
+        return render_template('agremenu.html')
+    elif request.method=='GET':
+        cur_01=conexion.cursor()
+        select_menu= "SELECT * FROM menu"
+        cur_01.execute(select_menu)
+        print(select_menu)
+        resultadosmenu=cur_01.fetchall()
+        resultadosmenujson=[]
+        print(resultadosmenu)
+        for r in resultadosmenu: 
+            resultadosmenujson.append({
+                "id":r[0],
+                "nombre":r[1],
+                "precio":r[2],
+                "restaurante":r[3],
+            })
+        print(resultadosmenujson)
+        return render_template('agremenu.html',resultadosmenu=resultadosmenujson)
 
 @app.route('/agreprod', methods = ['POST','GET'])
 def agreprod():
-    return render_template('agreprod.html')
+    if request.method == 'POST':
+        id_producto = request.form['id_producto']
+        precio_producto = request.form['precio_producto']
+        tipo_producto = request.form['tipo_producto']
+        restaurante_producto = request.form['restaurante_producto']
+        print(id_producto, precio_producto, tipo_producto, restaurante_producto)
+        cur_01=conexion.cursor()
+        insert_datos= "insert into producto (id_producto, precio_producto, tipo_producto, restaurante_producto) VALUES (:1, :2, :3, :4)"
+        print(insert_datos)
+        cur_01.execute(insert_datos,[id_producto, precio_producto, tipo_producto, restaurante_producto])
+        conexion.commit()
+        return render_template('agreprod.html')
+    elif request.method=='GET':
+        cur_01=conexion.cursor()
+        select_prod= "SELECT * FROM producto"
+        cur_01.execute(select_prod)
+        print(select_prod)
+        resultadosprod=cur_01.fetchall()
+        resultadosprodjson=[]
+        print(resultadosprod)
+        for r in resultadosprod: 
+            resultadosprodjson.append({
+                "id":r[0],
+                "precio":r[1],
+                "tipo":r[2],
+                "restaurante":r[3],
+            })
+        print(resultadosprodjson)
+        return render_template('agreprod.html',resultadosprod=resultadosprodjson)
 
 @app.route('/agreing', methods = ['POST','GET'])
 def agreing():
-    return render_template('agreing.html')
+    if request.method == 'POST':
+        id_ingrediente = request.form['id_ingrediente']
+        precio_ingrediente = request.form['precio_ingrediente']
+        restaurante_ingrediente = request.form['restaurante_ingrediente']
+        print(id_ingrediente, precio_ingrediente, restaurante_ingrediente)
+        cur_01=conexion.cursor()
+        insert_datos= "insert into ingrediente (id_ingrediente, precio_ingrediente, restaurante_ingrediente) VALUES (:1, :2, :3)"
+        print(insert_datos)
+        cur_01.execute(insert_datos,[id_ingrediente, precio_ingrediente, restaurante_ingrediente])
+        conexion.commit()
+        return render_template('agreing.html')
+    elif request.method=='GET':
+        cur_01=conexion.cursor()
+        select_ing= "SELECT * FROM ingrediente"
+        cur_01.execute(select_ing)
+        print(select_ing)
+        resultadosing=cur_01.fetchall()
+        resultadosingjson=[]
+        print(resultadosing)
+        for r in resultadosing: 
+            resultadosingjson.append({
+                "id":r[0],
+                "precio":r[1],
+                "restaurante":r[2],
+            })
+        print(resultadosingjson)
+        return render_template('agreing.html',resultadosing=resultadosingjson)
+
 
 @app.route('/modmenu', methods = ['POST','GET'])
 def modmenu():
-    return render_template('modmenu.html')
+    resultadosjson={
+                    "Id":"id",
+                    "Nombre":"nombre",
+                    "Precio":"precio",
+                    "Restaurante":"restaurante",        
+                    }
+    if request.method == 'POST':
+        id = request.form['id_menu']
+        nombre = request.form['nombre_menu']
+        precio = request.form['precio_menu']
+        restaurante = request.form['restaurante_menu']
+        update_menu='UPDATE menu set '
+        update_menu_bool=False
+        update_menu_atributos=[]
+        if nombre and nombre!='':
+                update_menu_bool=True
+                update_menu_atributos.append("nombre_menu='"+nombre+"'")
+        if precio and precio!='':
+                update_menu_bool=True
+                update_menu_atributos.append("precio_menu='"+precio+"'")
+        if restaurante and restaurante!='':
+                update_menu_bool=True
+                update_menu_atributos.append("restaurante_menu='"+restaurante+"'")
+        cur_01=conexion.cursor()
+        if update_menu_bool:
+                update_menu=update_menu+','.join(update_menu_atributos)+" Where id_menu = '"+id+"'"
+                print(update_menu)
+                cur_01.execute(update_menu)
+                conexion.commit()
+        return render_template('modmenu.html',form=resultadosjson)
+    elif request.method=='GET':
+        cur_01=conexion.cursor()
+        select_menu= "SELECT * FROM menu"
+        cur_01.execute(select_menu)
+        print(select_menu)
+        resultadosmenu=cur_01.fetchall()
+        resultadosmenujson=[]
+        print(resultadosmenu)
+        for r in resultadosmenu: 
+            resultadosmenujson.append({
+                "id":r[0],
+                "nombre":r[1],
+                "precio":r[2],
+                "restaurante":r[3],
+            })
+        print(resultadosmenujson)
+        return render_template('modmenu.html',resultadosmenu=resultadosmenujson)
+
+@app.route('/pizzamenu', methods = ['POST','GET'])
+def pizzamenu():
+    return render_template('pizzamenu.html')
+
+@app.route('/pizzamenucarta', methods = ['POST','GET'])
+def pizzamenucarta():
+    return render_template('pizzamenucarta.html')
+
+@app.route('/pizzamenucheck', methods = ['POST','GET'])
+def pizzamenucheck():
+    return render_template('pizzamenucheck.html')
+
+@app.route('/pizzamenuorden', methods = ['POST','GET'])
+def pizzamenuorden():
+    return render_template('pizzamenuorden.html')   
+
 
 if __name__ == '__main__':
     app.run(debug=True)
